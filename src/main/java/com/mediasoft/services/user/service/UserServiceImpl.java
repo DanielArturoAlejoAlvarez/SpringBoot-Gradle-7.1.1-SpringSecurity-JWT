3885788b660ab5cba5d998a8c81c14ff8ec7a281
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-19 15:34:36 -0500

    Adding roleSave method from UserServiceImpl

--- src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
+++ src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        return roleRepo.save(role);
     }
 
     @Override
