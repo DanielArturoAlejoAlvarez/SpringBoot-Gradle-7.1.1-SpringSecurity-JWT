ccfdf80f2b8c90d6d8e8df29861b75c8d891a11b
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-19 14:33:58 -0500

    Create User model

---
+++ src/main/java/com/mediasoft/services/user/domain/Role.java
@@ -0,0 +1,4 @@
+package com.mediasoft.services.user.domain;
+
+public class Role {
+}
---
+++ src/main/java/com/mediasoft/services/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.mediasoft.services.user.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Column(unique = true, length = 128, nullable = false)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @Column(length = 512)
+    private String avatar;
+    private Boolean status;
+    private Collection<Role> roles;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+}
