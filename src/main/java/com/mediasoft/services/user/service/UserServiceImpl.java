cb7d78c7beaf91f26764763fb8f4a461b876e9f9
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-19 15:19:29 -0500

    Creating UserServiceImpl service

--- src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
+++ src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
@@ -1,4 +1,35 @@
 package com.mediasoft.services.user.service;
 
+import com.mediasoft.services.user.domain.Role;
+import com.mediasoft.services.user.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements UserService{
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return null;
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+
+    }
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
 }
