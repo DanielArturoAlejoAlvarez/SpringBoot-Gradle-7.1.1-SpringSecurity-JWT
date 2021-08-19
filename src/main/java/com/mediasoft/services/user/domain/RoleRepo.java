c089ea8461d518e594ffb6f035b0bca7756a7047
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-19 14:58:14 -0500

    Create RoleRepo repository

---
+++ src/main/java/com/mediasoft/services/user/domain/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.mediasoft.services.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository<Role,Long> {
+    public Role findByName(String name);
+}
