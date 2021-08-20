f5027e9227d46d67e148982a35c977011fe2d4aa
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-20 00:00:35 -0500

    Creating getUser method in UserResource

--- src/main/java/com/mediasoft/services/user/api/UserResource.java
+++ src/main/java/com/mediasoft/services/user/api/UserResource.java
@@ -1,9 +1,14 @@
 package com.mediasoft.services.user.api;
 
+import com.mediasoft.services.user.domain.User;
 import com.mediasoft.services.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 }
