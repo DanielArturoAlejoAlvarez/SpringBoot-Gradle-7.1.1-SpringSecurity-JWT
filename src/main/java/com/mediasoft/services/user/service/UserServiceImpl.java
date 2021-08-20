d2532f2203d6ee98409d09af0521f97533f571e8
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-19 23:45:03 -0500

    Adding log info to our service

--- src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
+++ src/main/java/com/mediasoft/services/user/service/UserServiceImpl.java
@@ -22,16 +22,19 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} of user {}", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -39,11 +42,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
