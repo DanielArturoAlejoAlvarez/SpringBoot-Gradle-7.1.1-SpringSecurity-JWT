67252e111cba03ed7434c7d67c1ead9eeb5e6b1c
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-21 00:01:46 -0500

    Adding a Bean PasswordEncoder to our UserServiceApplication

--- src/main/java/com/mediasoft/services/user/UserServiceApplication.java
+++ src/main/java/com/mediasoft/services/user/UserServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +18,11 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
