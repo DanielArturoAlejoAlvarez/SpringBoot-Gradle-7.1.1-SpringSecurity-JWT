9e63c3a11d8d93f95d16022f8083d0029eb9f80f
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-21 13:11:30 -0500

    Adding constructor in the Autentication Manager to credentials and configure attemptAuthentication method

--- src/main/java/com/mediasoft/services/user/security/CustomAuthenticationFilter.java
+++ src/main/java/com/mediasoft/services/user/security/CustomAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.mediasoft.services.user.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -10,12 +13,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        //return super.attemptAuthentication(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        log.info("Your username is: {}", username);
+        log.info("Your password is: {}", password);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,password);
+        return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
