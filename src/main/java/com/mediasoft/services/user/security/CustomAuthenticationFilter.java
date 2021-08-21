609b82c1c826fc48035a847272806bbdd31e523c
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-21 12:57:45 -0500

    Creating a CustomAuthenticationFilter extends from UsernamePasswordAuthenticationFilter also add override methods

---
+++ src/main/java/com/mediasoft/services/user/security/CustomAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.mediasoft.services.user.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
