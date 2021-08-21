7757436f53c5107a004d9ab2f526d68223152c0d
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-20 23:54:19 -0500

    Add authentication based upon the custom UserDetailsService and configure BCryptPasswordEncoder

--- src/main/java/com/mediasoft/services/user/security/SecurityConfig.java
+++ src/main/java/com/mediasoft/services/user/security/SecurityConfig.java
@@ -6,14 +6,22 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
