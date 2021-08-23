29c274a991156cfb4be4b9883bc83ba1a7ee599f
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-23 14:25:16 -0500

    Configure Permissions Level in SecurityConfig

--- src/main/java/com/mediasoft/services/user/security/SecurityConfig.java
+++ src/main/java/com/mediasoft/services/user/security/SecurityConfig.java
@@ -37,8 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
--- src/main/java/com/mediasoft/services/user/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/mediasoft/services/user/security/filter/CustomAuthenticationFilter.java
@@ -67,6 +67,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         /*response.setHeader("access_token", access_token);
         response.setHeader("refresh_token", refresh_token);*/
 
+        Map<String,String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 
     }
 }
